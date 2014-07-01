c42bfd265c9b1d4c1f9af44ac02cb9b0c106ef31
David Cottingham <davidcottingham@gmail.com>
2014-07-01 15:45:38 -0400

    extension keyword search
    The extension's keyword "multi" followed by a user-defined category keyword followed by a search term searches all saved urls under that category keyword. Background script loads saved changes automatically. New tabs are opened using chrome's tab API. More testing is needed, but extensions seems to be in a basic functional state.

--- event.js
+++ event.js
@@ -1,8 +1,66 @@
-﻿chrome.omnibox.onInputEntered.addListener(function(text) {
+﻿//http://search.yahoo.com/search?p=%s
+
+var keywordMap = []; //empty array. will be used as a JSON array
+
+function loadKeywordMap() {
+	chrome.storage.sync.get({
+		keywordMap : keywordMap
+	}, loadCallback);
+}
+
+function loadCallback(items) {
+	keywordMap =  items.keywordMap; //set array
+	//console.log(keywordMap);
+}
+
+function queryArray(keyword) {
+	//console.log(keywordMap);
+	for (var i = 0; i < keywordMap.length; ++i) {
+		//console.log(keywordMap[i].text);
+		if (keyword === keywordMap[i].text) {
+			return keywordMap[i].sites;
+		}
+	}
+	return [];
+}
+
+function handleInput(text, disposition) {
+	//console.log(disposition);
 	var search = text.substr(0, text.indexOf(' '));
 	var terms = text.substr(text.indexOf(' ') + 1);
-	window.open("https://www.google.com/search?q=" + terms, "_blank");
-	console.log(search + " " + terms);
+	var sitesArray = queryArray(search);
+	if (sitesArray.length > 0) {
+		for (var i = 0; i < sitesArray.length; ++i) {
+			var url1 = sitesArray[i].substr(0, sitesArray[i].indexOf("%s"));
+			var url2 = sitesArray[i].substr(sitesArray[i].indexOf("%s") + 2);
+			if (url1.length > 0) {
+				var offset = 0;
+				if (i == 0 && disposition == "currentTab") {
+					chrome.tabs.update({url: url1 + encodeURIComponent(terms) + url2});
+				} else {
+					//window.open(url1 + terms + url2, "_blank");
+					chrome.tabs.create({url: url1 + encodeURIComponent(terms) + url2, active : false});
+				}
+			}
+		}
+	} else {
+		if (disposition == "currentTab") {
+			chrome.tabs.update({url: "https://www.google.com/search?q=" + encodeURIComponent(terms)});
+		} else {
+			chrome.tabs.create({url: "https://www.google.com/search?q=" + encodeURIComponent(terms)});
+		}
+	}
+}
+
+document.addEventListener('DOMContentLoaded', loadKeywordMap);
+chrome.omnibox.onInputEntered.addListener(handleInput);
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+	for (key in changes) {
+		if (key === "keywordMap") {
+			loadKeywordMap();
+			break;
+		}
+	}
 });
 
 //open options page
--- options.js
+++ options.js
@@ -1,4 +1,4 @@
-﻿var categories = []; //empty array. will be used as a JSON array
+﻿var keywordMap = []; //empty array. will be used as a JSON array
 
 var list = document.getElementById("categories"); //get the HTML datalist from the page
 var textBox = document.getElementById("category"); //get the HTML input for the category string from the page
@@ -10,7 +10,7 @@ var siteCount = 0;
 // Saves options to chrome.storage
 function saveOptions() {
 	chrome.storage.sync.set({
-		categoryList: categories
+		keywordMap: keywordMap //save keywordMap to settings
 	}, function() {
 		// Update status to let user know options were saved.
 		var status = document.getElementById("status");
@@ -18,6 +18,23 @@ function saveOptions() {
 		setTimeout(function() {
 			status.textContent = "";
 		}, 1000);
+	});
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restoreOptions() {
+	chrome.storage.sync.get({
+		keywordMap: keywordMap //pull keywordMap from saved data
+	}, function(items) {
+		keywordMap = items.keywordMap; //set array
+		//create HTML elements for the dropdown from the array data
+		for (var i = 0; i < keywordMap.length; ++i) { //for each saved keyword,
+			var newCat = document.createElement("option"); //create option element
+			newCat.id = "opt" + i.toString(); //change its id so can be referenced elsewhere
+			newCat.text = keywordMap[i].text; //set its text to that of the saved keyword
+			list.appendChild(newCat); //add it to the datalist
+		}
 	});
 }
 
@@ -43,9 +60,9 @@ function saveCat() {
 		}
 		
 		//check if editing keyword, rather than adding a new one
-		for (var i = 0; i < categories.length; ++i) { //for each saved keyword
-			if (categories[i].text === input) { //if input string matches a saved
-				categories[i].sites = sitesArray; //overwrite saved sites only (no need to re-write name or id)
+		for (var i = 0; i < keywordMap.length; ++i) { //for each saved keyword
+			if (keywordMap[i].text === input) { //if input string matches a saved
+				keywordMap[i].sites = sitesArray; //overwrite saved sites only (no need to re-write name or id)
 				saveOptions(); //go ahead and permanantly save options at this time
 				return; //exit function; no more work needed
 			}
@@ -53,10 +70,10 @@ function saveCat() {
 		
 		//creating a new keyword
 		var newCat = document.createElement("option"); //create an HTML option element so input can be added to dropdown
-		newCat.id = "opt" + categories.length.toString(); //form a string to use as html id of option element. prefixes array position with "opt"
+		newCat.id = "opt" + keywordMap.length.toString(); //form a string to use as html id of option element. prefixes array position with "opt"
 		newCat.text = input; //string-removed category name gets saved as the new element's text
 		
-		categories.push({"id" : newCat.id, "text" : newCat.text, "sites" : sitesArray}); //save this data to the array as new entry
+		keywordMap.push({"text" : newCat.text, "sites" : sitesArray}); //save this data to the array as new entry
 		list.appendChild(newCat); //add the newly-created option element to the datalist so can be accessed again by dropdown
 	}
 	saveOptions(); //go ahead and permanantly save options at this time
@@ -65,32 +82,15 @@ function saveCat() {
 //removes the category / search keyword from the dropdown and array
 function removeCat() {
 	var cat = textBox.value; //pull string from category text box
-	for (var i = 0; i < categories.length; ++i) { //loop through array
-		if (cat === categories[i].text) { //if input name matches a name of a saved category ...
-			list.removeChild(document.getElementById(categories[i].id)); //remove element from datalist using its id
-			categories.splice(i, 1); //splice the element out of the array
+	for (var i = 0; i < keywordMap.length; ++i) { //loop through array
+		if (cat === keywordMap[i].text) { //if input name matches a name of a saved category ...
+			list.removeChild(document.getElementById(keywordMap[i].id)); //remove element from datalist using its id
+			keywordMap.splice(i, 1); //splice the element out of the array
 			textBox.value = ""; //blank out text box
 			resetSitesList(); //call method to no longer show unneeded site inputs
 			break; //match found, so can break loop
 		}
 	}
-}
-
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restoreOptions() {
-	chrome.storage.sync.get({
-		categoryList: categories //pull categoryList from saved data
-	}, function(items) {
-		categories = items.categoryList; //set array
-		//create HTML elements for the dropdown from the array data
-		for (var i = 0; i < categories.length; ++i) { //for each saved keyword,
-			var newCat = document.createElement("option"); //create option element
-			newCat.id = "opt" + i.toString(); //change its id so can be referenced elsewhere
-			newCat.text = categories[i].text; //set its text to that of the saved keyword
-			list.appendChild(newCat); //add it to the datalist
-		}
-	});
 }
 
 //searches array as user types or selects item from dropdown
@@ -101,14 +101,14 @@ function selectedCat() {
 		//is removing all sites when cat input changes a desired behavior?
 			//maybe somehow check when WAS found input and changed. only remove all inputs in that case?
 		resetSitesList(); //reset list to one input when changing keyword
-		for (var i = 0; i < categories.length; ++i) { //iterate through saved array
-			if (cat === categories[i].text) { //if input matches name of the current element of the array
+		for (var i = 0; i < keywordMap.length; ++i) { //iterate through saved array
+			if (cat === keywordMap[i].text) { //if input matches name of the current element of the array
 				//create input boxes for every previously-saved site
-				for (var j = 0; j < categories[i].sites.length; ++j) { //for each site in sites array
+				for (var j = 0; j < keywordMap[i].sites.length; ++j) { //for each site in sites array
 					if (j == 0) { //if we are on the first site, just change the value of already-present first input
-						document.getElementById("site0").value = categories[i].sites[0];
+						document.getElementById("site0").value = keywordMap[i].sites[0];
 					} else { //if not on first site, need to create a new input and change its value to the saved one.
-						addSiteInput().value = categories[i].sites[j];
+						addSiteInput().value = keywordMap[i].sites[j];
 					}
 				}
 				if (siteCount < maxSites) { //if we are not yet at max sites allowed,
@@ -170,13 +170,16 @@ function removeSiteInput(node) {
 
 //DEBUG METHOD prints array to console for easy inpsection
 function logCats() {
-	console.log(categories);
+	console.log(keywordMap);
 }
 
-//DEBUG METHOD makes categories blank again
+//DEBUG METHOD makes keywordMap blank again
 function clearAll() {
-	categories = [];
+	keywordMap = [];
 	saveOptions();
+	chrome.storage.sync.clear(function () {
+		console.log("ok");
+	});
 }
 
 //event listeners for button clicks, page loaded, and input change in text area
